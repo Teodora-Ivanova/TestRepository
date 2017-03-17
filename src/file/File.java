107f56c17c4b9974ec18b7e3b14ed43057508f6f
DESKTOP-10SJ5T7\Teodora Ivanova <tedi199962@gmail.com>
2017-03-17 15:09:04 +0200

    second commit

--- src/file/File.java
+++ src/file/File.java
@@ -7,7 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+//hghghg
 public class File {
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner input = new Scanner(System.in);
